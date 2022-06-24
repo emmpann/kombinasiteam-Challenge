519c3f1a56e81ea2b0c19837a22c02465cb09130
emmpann <mhdepan@gmail.com>
2022-06-24 17:43:24 +0700

    update

--- src/main/java/efan/hackerrank/challange/Solution.java
+++ src/main/java/efan/hackerrank/challange/Solution.java
@@ -1,9 +1,41 @@
 package efan.hackerrank.challange;
 
 import java.io.*;
-import java.util.ArrayList;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 class Result {
+
+	/*
+	 * Complete the 'tentukanTeamLomba' function below.
+	 *
+	 * The function is expected to return a STRING.
+	 */
+
+	public static String tentukanTeamLomba(int kelasA, int kelasB, int kelasC) {
+		// Write your code here
+		if (kelasA >= 3 && kelasA <= 12) {
+			if (kelasB >= 3 && kelasB <= 12) {
+				if (kelasC >= 3 && kelasC <= 12) {
+					ArrayList<Integer> result = new ArrayList<Integer>(2);
+
+					// team Matematika (5 orang) syarat: 1 orang kelas A, 2 orang kelas B, 2 orang kelas C.
+					result.add(0, kombinasi(kelasA, 1) * kombinasi(kelasB, 2) * kombinasi(kelasC, 2));
+
+					// team Kimia (6 orang) syarat : 2 orang kelas A, 3 orang kelas B, 1 orang kelas C.
+					result.add(1, kombinasi(kelasA, 2) * kombinasi(kelasB, 3) * kombinasi(kelasC, 1));
+
+					return "Matematika: " + result.get(0) + " team\n" +
+							"Kimia: " + result.get(1) + " team";
+				}
+			}
+		}
+		return null;
+	}
 
 	public static Integer faktorial(int num) {
 		if (num == 0) {
@@ -17,48 +49,25 @@ class Result {
 		return faktorial(n) / (faktorial(r) * faktorial(n - r));
 	}
 
-	public static ArrayList<Integer> kombinasiTeam(int kelasA, int kelasB, int kelasC) {
-
-		// Constraints
-		if (kelasA >= 3 && kelasA <= 12) {
-			if (kelasB >= 3 && kelasB <= 12) {
-				if (kelasC >= 3 && kelasC <= 12) {
-					ArrayList<Integer> result = new ArrayList<Integer>(2);
-
-					// team Matematika (5 orang) syarat: 1 orang kelas A, 2 orang kelas B, 2 orang kelas C.
-					result.add(0, kombinasi(kelasA, 1) * kombinasi(kelasB, 2) * kombinasi(kelasC, 2));
-
-					// team Kimia (6 orang) syarat : 2 orang kelas A, 3 orang kelas B, 1 orang kelas C.
-					result.add(1, kombinasi(kelasA, 2) * kombinasi(kelasB, 3) * kombinasi(kelasC, 1));
-
-					return result;
-				}
-			}
-		}
-
-		return new ArrayList<>();
-	}
 }
 
 public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-		int kelasA = Integer.parseInt(bufferedReader.readLine().trim());
-		int kelasB = Integer.parseInt(bufferedReader.readLine().trim());
-		int kelasC = Integer.parseInt(bufferedReader.readLine().trim());
+		int a = Integer.parseInt(bufferedReader.readLine().trim());
 
-		ArrayList<Integer> result = new ArrayList<>(2);
-		result = Result.kombinasiTeam(kelasA, kelasB, kelasC);
+		int b = Integer.parseInt(bufferedReader.readLine().trim());
 
-		bufferedWriter.write("Matematika: " + result.get(0) + " team\n" +
-				"Kimia: " + result.get(1));
+		int c = Integer.parseInt(bufferedReader.readLine().trim());
+
+		String result = Result.tentukanTeamLomba(a, b, c);
+
+		bufferedWriter.write(result);
+		bufferedWriter.newLine();
 
 		bufferedReader.close();
 		bufferedWriter.close();
 	}
 }
-
-
-
--- src/test/java/efan/hackerrank/challange/SolutionTest.java
+++ src/test/java/efan/hackerrank/challange/SolutionTest.java
@@ -8,11 +8,6 @@ import java.util.Arrays;
 class SolutionTest {
 
 	@Test
-	void faktorialTest() {
-		System.out.println(Result.faktorial(3));
-	}
-
-	@Test
 	void kombinasiTest() {
 		Assertions.assertEquals(10, Result.kombinasi(5, 2));
 		Assertions.assertEquals(84, Result.kombinasi(9, 3));
@@ -20,20 +15,32 @@ class SolutionTest {
 	}
 
 	@Test
-	void kombinasiTeamTest() {
-		//Assertions.assertArrayEquals(new int[] {60, 60}, Result.kombinasiTeam(11,6,15));
-		Result.kombinasiTeam(11,6,12).forEach((e) -> {
-			System.out.print(e + ", ");
-		});
+	void testCase1() {
+		Assertions.assertEquals("Matematika: 4158 team\n" +
+				"Kimia: 1260 team", Result.tentukanTeamLomba(3, 7, 12));
 	}
 
 	@Test
-	void testCase1() {
-		System.out.println(Result.kombinasiTeam(5, 3, 4));
-		System.out.println(Result.kombinasiTeam(3, 7, 12));
-		System.out.println(Result.kombinasiTeam(6, 9, 8));
-		System.out.println(Result.kombinasiTeam(7, 11, 4));
-		System.out.println(Result.kombinasiTeam(12, 9, 3));
-		System.out.println(Result.kombinasiTeam(12, 8, 6));
+	void testCase2() {
+		Assertions.assertEquals("Matematika: 6048 team\n" +
+				"Kimia: 10080 team", Result.tentukanTeamLomba(6, 9, 8));
+	}
+
+	@Test
+	void testCase3() {
+		Assertions.assertEquals("Matematika: 2310 team\n" +
+				"Kimia: 13860 team", Result.tentukanTeamLomba(7, 11, 4));
+	}
+
+	@Test
+	void testCase4() {
+		Assertions.assertEquals("Matematika: 1296 team\n" +
+				"Kimia: 16632 team", Result.tentukanTeamLomba(12, 9, 3));
+	}
+
+	@Test
+	void testCase5() {
+		Assertions.assertEquals("Matematika: 5040 team\n" +
+				"Kimia: 22176 team", Result.tentukanTeamLomba(12, 8, 6));
 	}
 }
